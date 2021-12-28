18ca1cf0d072609cf3d00da8b1216a7f5ed7521f
Facundo <facufas1401@gmail.com>
2021-12-28 12:49:29 -0300

    proyecto terminado

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,5 @@
 import styles from "./Search.module.css";
 import { FaSearch } from "react-icons/fa";
-import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useQuery } from "../hooks/useQuery";
 
@@ -8,16 +7,10 @@ export function Search() {
   const query = useQuery();
   const search = query.get("search");
 
-  const [searchText, setSearchText] = useState("");
   const history = useHistory();
-
-  useEffect(() => {
-    setSearchText(search || " ");
-  }, [search]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    history.push("/?search=" + searchText);
   };
 
   return (
@@ -26,12 +19,18 @@ export function Search() {
         <input
           className={styles.searchInput}
           type="text"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
+          value={search ?? ""}
+          autofocus
+          placeholder="Title"
+          aria-label="Search Movies"
+          onChange={(e) => {
+            const value = e.target.value;
+
+            history.push("/?search=" + value);
+          }}
         />
-        <button className={styles.searchButton} type="submit">
-          <FaSearch size={20} />
-        </button>
+
+        <FaSearch size={20} color="black" className={styles.searchButton} />
       </div>
     </form>
   );
--- src/index.js
+++ src/index.js
@@ -3,29 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
 
-ReactDOM.render(
-  // <Componente titulo="Titulo" contenido="Hola desde React" />,
-  // <Componente titulo="Titulo">Hola desde React</Componente> ,
-  <App />,
-  document.getElementById("root")
-);
-
-// const contenido = (
-//   <div>
-//     <h1>Titulo</h1>
-//     <div>Hola desde React</div>
-//   </div>
-// );
-// function Componente({ titulo, children }) {
-
-//   // const titulo = props.titulo;
-//   // const contenido = props.contenido;
-
-//   // const { titulo, contenido } = props;
-//   return (
-//     <div>
-//       <h1>{titulo} </h1>
-//       <div>{children} </div>
-//     </div>
-//   );
-// }
+ReactDOM.render(<App />, document.getElementById("root"));
